4620b45e757d08de854b0e0689e67c3338d42340
behilalemu <91044960+Behil-Alemu@users.noreply.github.com>
2022-09-19 16:25:18 -0400

    Virual Env created and requirements added

---
+++ app.py
@@ -0,0 +1,4 @@
+from flask import Flask, render_template, request
+import requests
+
+API_BASE_URL = "https://collectionapi.metmuseum.org/public/collection/v1 "
--- static/art.js
+++ static/art.js
@@ -5,15 +5,16 @@ let BASE_URL ="https://collectionapi.metmuseum.org/public/collection/v1";
 let five_art_pieces=[]
 async function five_art_piece(){
     let alldata = await $.getJSON(`${BASE_URL}/search?hasImages=true&q=Paintings`);
-    for (let i = 1; i < 10; i++) {
+    for (let i = 0; i < 20; i++) {
         let randomIdx = Math.floor(Math.random()*alldata.objectIDs.length)
         let randomImg = await $.getJSON(`${BASE_URL}/objects/${alldata.objectIDs[randomIdx]}`);
 
     five_art_pieces.push(randomImg)
     let imageSpliced = five_art_pieces.splice(0,1)
+    console.log(imageSpliced)
 
     imageSpliced.map(art => {
-        $("div").append(`<img  class="img-fluid " src="${art.primaryImage}"></img>`)
+        $("div").append(`<img  class="img-fluid " src="${art.primaryImageSmall}"></img>`)
     })
     } 
 
