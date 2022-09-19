ab3d1e1e7375532be4208e211488410770687925
behilalemu <91044960+Behil-Alemu@users.noreply.github.com>
2022-09-19 00:02:30 -0400

    fetch the API data first trial

--- static/art.js
+++ static/art.js
@@ -1,1 +1,22 @@
-const BASE_URL =""
+
+$(function() {
+let BASE_URL ="https://collectionapi.metmuseum.org/public/collection/v1";
+
+let five_art_pieces=[]
+async function five_art_piece(){
+    let alldata = await $.getJSON(`${BASE_URL}/search?hasImages=true&q=Paintings`);
+    for (let i = 1; i < 10; i++) {
+        let randomIdx = Math.floor(Math.random()*alldata.objectIDs.length)
+        let randomImg = await $.getJSON(`${BASE_URL}/objects/${alldata.objectIDs[randomIdx]}`);
+
+    five_art_pieces.push(randomImg)
+    let imageSpliced = five_art_pieces.splice(0,1)
+
+    imageSpliced.map(art => {
+        $("div").append(`<img  class="img-fluid " src="${art.primaryImage}"></img>`)
+    })
+    } 
+
+}
+five_art_piece()
+});
