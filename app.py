153476a1c53081e704d636e06196b032c5eb1a87
behilalemu <91044960+Behil-Alemu@users.noreply.github.com>
2022-09-20 11:26:51 -0400

    API call on python did not work

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
+from email.mime import image
 from flask import Flask, request, render_template,  redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from models import db,  connect_db, User, Post,Likes,Inspiration
+import json
 import requests
 
 app = Flask(__name__)
@@ -15,9 +17,39 @@ debug = DebugToolbarExtension(app)
 
 
 connect_db(app)
+db.create_all()
 
-API_BASE_URL = "https://collectionapi.metmuseum.org/public/collection/v1 "
+# API_BASE_URL = "https://collectionapi.metmuseum.org/public/collection/v1 "
 
+# def search_art(searched):
+#     res = requests.get(f"{API_BASE_URL}/search?hasImages=true&q=Paintings&p={searched}")
+#     data = res.json()
+#     image= data["objectIDs"][0]["primaryImage"]
+#     return image
+
+# @app.route('/')
+# def show_search_form():
+#     return render_template("base.html")
+
+
+# @app.route('/show_searched_art')
+# def get_art():
+#     searched = request.args["image"]
+#     image_url = search_art(searched)
+#     # import pdb; pdb.set_trace()
+#     return render_template('base.html', image_url=image_url)
 
 # for i in range(len.lenghtofAPI)
 # api url with object id randrange(length)
+
+
+response = requests.get("https://collectionapi.metmuseum.org/public/collection/v1/search?hasImages=true&q=Paintings&p=African")
+data = response.json()
+url = data["objectIDs"][0]
+
+
+
+res_id = requests.get("https://collectionapi.metmuseum.org/public/collection/v1/objects/{url}")
+
+data_url = res_id.json()
+image_URL= data_url["primaryImage"]
--- static/art.js
+++ static/art.js
@@ -1,23 +1,23 @@
 
-$(function() {
-let BASE_URL ="https://collectionapi.metmuseum.org/public/collection/v1";
+// $(function() {
+// let BASE_URL ="https://collectionapi.metmuseum.org/public/collection/v1";
 
-let five_art_pieces=[]
-async function five_art_piece(){
-    let alldata = await $.getJSON(`${BASE_URL}/search?hasImages=true&q=Paintings`);
-    for (let i = 0; i < 20; i++) {
-        let randomIdx = Math.floor(Math.random()*alldata.objectIDs.length)
-        let randomImg = await $.getJSON(`${BASE_URL}/objects/${alldata.objectIDs[randomIdx]}`);
+// let five_art_pieces=[]
+// async function five_art_piece(){
+//     let alldata = await $.getJSON(`${BASE_URL}/search?hasImages=true&q=Paintings`);
+//     for (let i = 0; i < 20; i++) {
+//         let randomIdx = Math.floor(Math.random()*alldata.objectIDs.length)
+//         let randomImg = await $.getJSON(`${BASE_URL}/objects/${alldata.objectIDs[randomIdx]}`);
 
-    five_art_pieces.push(randomImg)
-    let imageSpliced = five_art_pieces.splice(0,1)
-    console.log(imageSpliced)
+//     five_art_pieces.push(randomImg)
+//     let imageSpliced = five_art_pieces.splice(0,1)
+//     console.log(imageSpliced)
 
-    imageSpliced.map(art => {
-        $("div").append(`<img  class="img-fluid " src="${art.primaryImageSmall}"></img>`)
-    })
-    } 
+//     imageSpliced.map(art => {
+//         $("div").append(`<img  class="img-fluid " src="${art.primaryImageSmall}"></img>`)
+//     })
+//     } 
 
-}
-five_art_piece()
-});
+// }
+// five_art_piece()
+// });
