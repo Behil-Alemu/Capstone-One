4cffa93507a074d4b98a40692f12e0e78bf52777
behilalemu <91044960+Behil-Alemu@users.noreply.github.com>
2022-10-10 21:38:04 -0400

    config DATABASE_URL

--- app.py
+++ app.py
@@ -22,7 +22,8 @@ CURR_USER_KEY = "curr_user"
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///capstone_db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+    'DATABASE_URL', 'postgresql:///capstone_db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'riveroflove986')
